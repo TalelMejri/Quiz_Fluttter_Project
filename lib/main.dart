e00c4d9233ccefbdddd0e81a5e7d005c200ea68a
TalelMejri <talelmejri8@gmail.com>
2023-08-24 13:16:33 +0100

    make first Design

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+//import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 void main(){
   runApp(const MyApp());
 }
@@ -32,15 +32,36 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return  Scaffold(
       backgroundColor: Colors.deepPurple,
-      bottomNavigationBar:  CurvedNavigationBar(
-        backgroundColor: Colors.deepPurple,
-        color: Colors.deepPurple.shade200,
-        items: <Widget> [
-           Icon(Icons.home,color: Colors.white),
-           Icon(Icons.account_balance,color:Colors.white),
-           Icon(Icons.settings,color: Colors.white,),
+      appBar: AppBar(title: Text(widget.title),),
+      body: Padding(padding: const EdgeInsets.all(20),
+      child: Column(
+         crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(flex:5,child: Column(children: [
+            Image.asset("images/image-1.jpg"),
+            const SizedBox(height: 20,),
+            const Text("test",textAlign: TextAlign.center,style: TextStyle(fontSize: 24.0),)
+          ],)
+          ),
+          Expanded(child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: ElevatedButton(onPressed: (){},style: ElevatedButton.styleFrom(backgroundColor: Colors.green),child:const Text("Correct"),)
+          ),),
+            Expanded(child: Padding(
+            padding:const EdgeInsets.symmetric(vertical: 10),
+            child: ElevatedButton(onPressed: (){},style: ElevatedButton.styleFrom(backgroundColor: Colors.red),child: const Text("Incorrect"))
+          ),)
         ],
-      ),
+      ),),
+      // bottomNavigationBar:  CurvedNavigationBar(
+      //   backgroundColor: Colors.deepPurple,
+      //   color: Colors.deepPurple.shade200,
+      //   items: <Widget> [
+      //      Icon(Icons.home,color: Colors.white),
+      //      Icon(Icons.account_balance,color:Colors.white),
+      //      Icon(Icons.settings,color: Colors.white,),
+      //   ],
+      // ),
     );
   }
 }
